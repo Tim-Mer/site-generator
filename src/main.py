556c357d2f75edbc420ca04b02805ba655877760
Tim-Mer <timmymerriman@gmail.com>
2025-05-17 18:33:53 -0700

    Trying new layout

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def main():
     basepath = "/"
     if len(sys.argv) >= 2:
         basepath = sys.argv[1]
-    content_path = os.path.join("content")
+    content_path = os.path.join("site-generator")
     public_path = os.path.join("docs")
     static_path = os.path.join("static")
 
